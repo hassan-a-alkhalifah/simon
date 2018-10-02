7de9949022dc41d75557318338ed46c13adaa7a4
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-02 00:52:49 -0700

    add backend logic to handle basic button clicks that store random and user colors selected in relevant arrays

--- src/js/simon.js
+++ src/js/simon.js
@@ -1,4 +1,18 @@
 // Business Logic
-export function play() {
-  console.log("test");
+export function SimonGame() {
+  this.simonPattern = [];
+  this.playerPattern = [];
 }
+
+SimonGame.prototype.play = function() {
+  var simonRandomCurrentChoice = Math.floor((Math.random() * 4) + 1);
+  if(simonRandomCurrentChoice == 1) {
+    this.simonPattern.push("green");
+  } else if(simonRandomCurrentChoice == 2) {
+    this.simonPattern.push("red");
+  } else if(simonRandomCurrentChoice == 3) {
+    this.simonPattern.push("yellow");
+  } else {
+    this.simonPattern.push("blue");
+  }
+};
