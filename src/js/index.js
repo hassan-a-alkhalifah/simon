9dbe20c51d3f8e6459ed7865f5d2bf148b43c5f8
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-02 00:51:14 -0700

    add UI logic for button clicks on all 5 available buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,29 @@
 import './../scss/styles.scss';
-import { play } from './simon.js';
 import $ from 'jquery';
+import {SimonGame} from './simon.js';
+import { play } from './simon.js';
 
 // UI Logic
 $(document).ready(function(){
-  play();
+  var newGame = new SimonGame();
+  $("#logo").click(function() {
+    newGame.play();
+    console.log(newGame);
+  });
+  $("#green-btn").click(function() {
+    newGame.playerPattern.push("green");
+    console.log(newGame);
+  });
+  $("#red-btn").click(function() {
+    newGame.playerPattern.push("red");
+    console.log(newGame);
+  });
+  $("#yellow-btn").click(function() {
+    newGame.playerPattern.push("yellow");
+    console.log(newGame);
+  });
+  $("#blue-btn").click(function() {
+    newGame.playerPattern.push("blue");
+    console.log(newGame);
+  });
 });
